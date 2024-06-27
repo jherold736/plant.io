872ae2f0669dba21b4be21b58452b091d1e8ebc0
Jebedaja <77245826+Jebedaja@users.noreply.github.com>
2024-06-27 03:22:23 +0200

    nowy account page
    nowy look

--- ionic kopia nr 2/ionic3copyy/plant-me/src/app/pages/tabs/account/account.page.ts
+++ ionic kopia nr 2/ionic3copyy/plant-me/src/app/pages/tabs/account/account.page.ts
@@ -1,17 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.page.html',
   styleUrls: ['./account.page.scss'],
 })
-export class AccountPage implements OnInit {
+export class AccountPage {
+  profile = {
+    name: '',
+    email: ''
+  };
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  updateProfile() {
+    console.log('Profile updated!', this.profile);
+    }
+    
 
+  changePassword() {
+      console.log('Change password clicked');
+      
+    }
+  
+  logout() {
+      console.log('Logout clicked');
+    
+  }
 }
 
 // głowny moduł strony konta, def szablonu html i css do niego
